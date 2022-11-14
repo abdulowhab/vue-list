a91638b0081d58593a230db27d5d568b274c36ff
Md. Abdul Owhab <owhab@pigeon-soft.com>
2022-11-14 10:51:07 +0600

    showing data from api

---
+++ src/components/Events.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="event-card">
+      <span>@ {{event.time}} on {{event.date}}</span>
+      <h1>{{event.title}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line
+  name: 'Events',
+  props: {
+    event: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.event-card{
+  padding: 20px;
+  width: 40%;
+  cursor: pointer;
+  border: 1px solid black;
+  margin: 10px auto;
+}
+
+.event-card:hover{
+  transform: scale(1.01);
+  box-shadow: 0 3px 1px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Events v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// eslint-disable-next-line
+import Events from '@/components/Events.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Events
+  },
+  data(){
+    return{
+      events: [
+            { id: 5928101,
+                category: "animal welfare",
+                title: "Cat Adoptation Day",
+                description: "FInd your new feline friend at this event",
+                location: "Meow Town",
+                date: "January 28, 2022",
+                time: "12:00",
+                petsAllowed: true,
+                organizer: "Kal Laydee"
+            },
+            { id: 59281001,
+                category: "Vue Js Conf",
+                title: "Vue JS Conference",
+                description: "FInd your new feline friend at this event",
+                location: "Town Hall, Dhaka",
+                date: "January 30, 2022",
+                time: "03:00",
+                petsAllowed: true,
+                organizer: "Abdul Owhab"
+            },
+            { id: 592801001,
+                category: "React Conf",
+                title: "React JS Conference",
+                description: "FInd your new feline friend at this event",
+                location: "Town Hall, Dhaka",
+                date: "January 30, 2022",
+                time: "03:00",
+                petsAllowed: true,
+                organizer: "Abdul Owhab"
+            },
+            { id: 592801001,
+                category: "Laravel Conf",
+                title: "Laravel Conference",
+                description: "FInd your new feline friend at this event",
+                location: "Town Hall, Dhaka",
+                date: "January 30, 2022",
+                time: "03:00",
+                petsAllowed: true,
+                organizer: "Abdul Owhab"
+            },
+        ]
+    }
+  }
+}
+</script>
